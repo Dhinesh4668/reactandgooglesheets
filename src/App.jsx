9e16d79ce609c037680d9d5b07737f2741f91570
Dhinesh4668 <dhinesh4668@gmail.com>
2023-09-27 22:01:57 +0530

    formate doc 🥶🥶

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import axios from 'axios'
-const App =()=>{
+const App = () => {
 
   // from data
   const [name, setName] = useState('')
@@ -24,14 +24,14 @@ const App =()=>{
     }
 
 
-    const Data={
-      Name:name,
-      Age:age,
-      Department:dep,
-      Year:year,
-      Email:email
+    const Data = {
+      Name: name,
+      Age: age,
+      Department: dep,
+      Year: year,
+      Email: email
     }
-    axios.post('https://sheet.best/api/sheets/a12472ae-5d1e-4117-82f9-244940636c40',Data).then((responce)=>{
+    axios.post('https://sheet.best/api/sheets/a12472ae-5d1e-4117-82f9-244940636c40', Data).then((responce) => {
       // console.log(responce);
       setName('')
       setAge('')
@@ -42,7 +42,7 @@ const App =()=>{
   };
 
 
-  return(
+  return (
     <div className="container mt-5">
       <p className="h2" style={{
         fontVariant: 'simplified',
@@ -54,30 +54,30 @@ const App =()=>{
 
       <form autoComplete="on" className="form-group" onSubmit={handleSubmit}>
         <label>Name</label> <br />
-        <input type="text" className="form-control" placeholder="sam" 
-        onChange={(e)=>setName(e.target.value)} value={name}
+        <input type="text" className="form-control" placeholder="sam"
+          onChange={(e) => setName(e.target.value)} value={name}
         />
         <label>Age</label> <br />
-        <input type="number" className="form-control" placeholder="20" 
-        onChange={(e)=>setAge(e.target.value)} value={age}
+        <input type="number" className="form-control" placeholder="20"
+          onChange={(e) => setAge(e.target.value)} value={age}
         />
         <label>Department</label> <br />
-        <input type="text"  placeholder="Infromation Technology " className="form-control" 
-        onChange={(e)=>setDep(e.target.value)} value={dep}
+        <input type="text" placeholder="Infromation Technology " className="form-control"
+          onChange={(e) => setDep(e.target.value)} value={dep}
         />
         <label>Year</label> <br />
-        <input type="number" className="form-control" placeholder="4" 
-        onChange={(e)=>setYear(e.target.value)} value={year}
+        <input type="number" className="form-control" placeholder="4"
+          onChange={(e) => setYear(e.target.value)} value={year}
         />
-        <label>Email</label><br/>
-        <input type="email" className="form-control" placeholder="test@mail.com" 
-        onChange={(e)=>setEmail(e.target.value)} value={email}
+        <label>Email</label><br />
+        <input type="email" className="form-control" placeholder="test@mail.com"
+          onChange={(e) => setEmail(e.target.value)} value={email}
         /> <br />
         {/* submit button */}
         <div style={{
-          
+
         }}
-        className="d-grid gap-2"
+          className="d-grid gap-2"
         >
           <button className="btn btn-primary" type="submit" >Submit</button>
         </div>
