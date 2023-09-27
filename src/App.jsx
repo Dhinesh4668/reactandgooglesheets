ba7378439849049195742c44c0c372c70320e6b0
Dhinesh4668 <dhinesh4668@gmail.com>
2023-09-27 21:15:23 +0530

    from  initial 📄

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =()=>{
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  // from data
+  const [name,setName] = useState('')
+  const [age,setAge] = useState('')
+  const [dep,setDep] = useState('')
+  const [year,setYear] = useState('')
+
+  // submiting event
+  return(
+    <div className="container">
+      <h1>Google from data using the REATC and RESTapi 📄</h1>
+      <br />
+
+
+      <form autoComplete="off" className="form-group">
+        <label>Name</label> <br />
+        <input type="text" placeholder="sam" required/>
+        <br/>
+        <label>Age</label> <br />
+        <input type="number" placeholder="20" required/> <br />
+        <label>Department</label> <br />
+        <input type="text" placeholder="Infromation Technology " required/> <br />
+        <label>Year</label> <br />
+        <input type="number" placeholder="4" required/> <br />
+        <label>Email</label> <br/>
+        <input type="email" placeholder="test@mail.com" required/>
+      </form>
+    </div>
   )
 }
 
